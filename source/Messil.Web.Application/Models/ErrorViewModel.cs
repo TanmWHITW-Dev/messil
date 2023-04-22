872df53d741fee130a690222764789b4e382a680
TanmWHITW-Dev <tanmwhitw.gh@gmail.com>
2023-04-22 06:33:57 -0700

    #1 Created MVC Project
    Initial commit for project creation

---
+++ source/Messil.Web.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Messil.Web.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
