0f58022d86eb44ca2a20f4c4213a1e722592fbf5
TanmWHITW-Dev <tanmwhitw.gh@gmail.com>
2023-04-23 05:06:09 -0700

    #4 Added 2 Libraries
    Application.Core and Application.Infrastructure were added as project references to the clean solution

--- source/Messil.Web.Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Messil.Web.Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
