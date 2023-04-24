f51e8bc07d0ed0c4bf8da743ffb25295205f9c0f
TanmWHITW-Dev <tanmwhitw.gh@gmail.com>
2023-04-24 10:06:53 -0700

    #5 Fucked up a bit

---
+++ source/Messil.Shared/Entities/Message/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Messil.Shared.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string SenderId { get; set; } = string.Empty;
+        public User Sender { get; set; } = new User();
+        public string RecieverId { get; set; } = string.Empty;
+        public User Reciever { get; set;} = new User();
+    }
+}   
